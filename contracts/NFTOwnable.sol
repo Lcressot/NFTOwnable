8af8d896ebd8b142ed59fc0e8eb90fc315a83650
Lcressot <Lcressot@gmail.com>
2021-10-29 16:54:20 +0200

    added MIT license

--- contracts/NFTOwnable.sol
+++ contracts/NFTOwnable.sol
@@ -1,46 +1,8 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 //import '@openzeppelin/contracts/access/Ownable.sol';
 import './Ownable.sol'; // lasted version of openzeppelin Ownable
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-
-
-/**
- * @dev Module to make the ownership of a smart contract a tradable ERC721 token
- * NOTE: This module should only be deployed by a contract implementing NFTOwnable
- */ 
-contract ERC721Ownership is ERC721{
-    uint constant NFT_ID = 1;
-    NFTOwnable public _ownedContract;
-    
-    /**
-     * @dev Mint the single token of the contract to the initial owner at deployment
-     */    
-    constructor(address initialOwner) ERC721("ERC721Ownership", "OWN") {
-        _mint(initialOwner, NFT_ID);
-        _ownedContract = NFTOwnable(msg.sender);                
-    }
-
-    function ownedContract() external view returns (address){
-        return address(_ownedContract);
-    }
-
-    /**
-     * @dev Transfer of the token also causes transfer of the ownership of _ownedContract
-     */
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal override {
-        require(to != address(0), "ERC721Ownership: transfer to the zero address");
-        if(address(_ownedContract)!=address(0)){
-            _ownedContract.ownershipTransfer(to);
-        }
-    }  
-
-}
 
 
 /**
